f99da95ce35c54a344bbeee8174f0e9bb6b3e673
aditya <adityahongalwork@gmail.com>
2024-01-01 16:36:39 +0530

    Describing the UI in React - First Component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Profile from './Profile.js';
+import Gallery from './Gallery.js';
+
 // import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Gallery />
+    <Profile />
   </React.StrictMode>
 );
 
