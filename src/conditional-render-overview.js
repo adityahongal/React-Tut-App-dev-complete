f9b600d3e8bd704be9c6686ebe34d409f9127093
aditya <adityahongalwork@gmail.com>
2024-01-01 17:39:24 +0530

    Conditional Rendering Overviewx

--- src/conditional-render-overview.js
+++ src/conditional-render-overview.js
@@ -1,3 +1,11 @@
+// Conditional rendering 
+
+// Your components will often need to display different things depending on different conditions. 
+// In React, you can conditionally render JSX using JavaScript syntax like if statements, &&, and ? : operators.
+
+// In this example, the JavaScript && operator is used to conditionally render a checkmark:
+
+
 function Item({ name, isPacked }) {
   return (
     <li className="item">
