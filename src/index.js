acc8921ab34e7f6019cc2e9705494ce9590b72b7
aditya <adityahongalwork@gmail.com>
2024-01-01 17:37:47 +0530

    Conditional Rendering Overview

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import App from './App';
 // import Profile from './profile.js';
 // import Gallery from './gallery.js';
 // import TodoList from './javascript-in-JSX.js';
-import Profile from './passing-props-to-children';
-// import App from './App';
+// import Profile from './passing-props-to-children';
+import PackingList from './conditional-render-overview.js'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +15,8 @@ root.render(
     {/* <Gallery /> */}
     {/* <Profile /> */}
     {/* <TodoList /> */}
-    <Profile/>
+    {/* <Profile/> */}
+    <PackingList/>
   </React.StrictMode>
 );
 
